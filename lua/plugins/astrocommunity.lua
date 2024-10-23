b1f25235666c483f8ac4b17d258e2bd6a0774e79
tpp <tp.phan@laudert.com>
2024-10-23 14:35:02 +0700

    Update astrocommunity package

--- lua/plugins/astrocommunity.lua
+++ lua/plugins/astrocommunity.lua
@@ -33,43 +33,48 @@ return {
   { import = "astrocommunity.color.twilight-nvim" },
   { import = "astrocommunity.color.headlines-nvim" },
   { import = "astrocommunity.color.huez-nvim" },
-  -- { import = "astrocommunity.recipes.vscode-icons" },
 
+  -- UI
   { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
   { import = "astrocommunity.bars-and-lines.scope-nvim" },
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.recipes.telescope-nvchad-theme" },
-
-
-  { import = "astrocommunity.project.project-nvim" },
-  -- { import = "astrocommunity.project.project-nvim" },
-  { import = "astrocommunity.project.projectmgr-nvim" },
   { import = "astrocommunity.terminal-integration.flatten-nvim" },
-  -- { import = "astrocommunity.workflow.hardtime-nvim" },
 
+  -- Editor
   { import = "astrocommunity.editing-support.text-case-nvim" },
   { import = "astrocommunity.editing-support.vim-move" },
   { import = "astrocommunity.editing-support.multicursors-nvim" },
   { import = "astrocommunity.quickfix.nvim-bqf" },
-  { import = "astrocommunity.search.nvim-spectre" },
 
+  { import = "astrocommunity.search.nvim-spectre" },
   { import = "astrocommunity.motion.vim-matchup" },
   { import = "astrocommunity.scrolling.mini-animate" },
 
-  { import = "astrocommunity.git.diffview-nvim" },
-  { import = "astrocommunity.git.git-blame-nvim" },
-
+  -- Package
   { import = "astrocommunity.pack.proto" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.svelte" },
   { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.php" },
 
+  -- Support tool
   { import = "astrocommunity.programming-language-support.web-tools-nvim" },
   { import = "astrocommunity.code-runner.compiler-nvim" },
 
+  { import = "astrocommunity.git.diffview-nvim" },
+  { import = "astrocommunity.git.git-blame-nvim" },
+
+  { import = "astrocommunity.project.project-nvim" },
+  { import = "astrocommunity.project.projectmgr-nvim" },
+
+  -- Debug
   { import = "astrocommunity.debugging.telescope-dap-nvim" },
   { import = "astrocommunity.debugging.nvim-dap-repl-highlights" },
   { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+
+  -- Support action
+  { import = "astrocommunity.workflow.hardtime-nvim" },
 }
